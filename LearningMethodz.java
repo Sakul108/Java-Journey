a58b9fa55d2c1530cd5c41176fcadc0883aceff4
Sakul108 <sakulkhatri108@gmail.com>
2025-05-24 13:08:11 +0545

    Add files via upload
    This is me trying to understand how methods work in JAVA. As of now, the program lacks the ability of picking variables from the user. Will work on this later.

---
+++ LearningMethodz.java
@@ -0,0 +1,18 @@
+public class LearningMethodz{
+
+    public static void main(String[] args) {
+         
+        int result  = diff();
+       System.out.println("The result is "+ result);
+    }
+
+
+public static int diff(){             // all methods must be declared within a class
+
+    int x = 5;
+    int y = 3;
+    int difference = x-y;
+    return difference;
+}
+
+}
